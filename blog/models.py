19d6145e12a236e71d02726088aeafa0272b8ed1
chrislo42 <chris42.lopez@gmail.com>
2017-01-12 17:03:17 +0100

    ajout categorie

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    category = models.ForeignKey('Category')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -32,3 +33,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
