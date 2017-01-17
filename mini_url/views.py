56183edd4d29aca071d9394c5efc3e67ea7d8507
chrislo42 <chris42.lopez@gmail.com>
2017-01-17 23:45:21 +0100

    vues génériques pour edition et suppression miniurl

--- mini_url/views.py
+++ mini_url/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import MiniURL
 from .forms import MiniURLForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
@@ -27,3 +29,15 @@ def miniurl_redir(request, code):
     miniurl.access += 1
     miniurl.save()
     return redirect(miniurl.longUrl, permanent=False)
+
+class URLUpdate(UpdateView):
+    model = MiniURL
+    template_name = 'mini_url/miniurl_edit.html'
+    form_class = MiniURLForm
+    success_url = reverse_lazy(miniurl_list)
+
+class URLDelete(DeleteView):
+    model = MiniURL
+    context_object_name = "mini_url"
+    template_name = 'mini_url/miniurl_del.html'
+    success_url = reverse_lazy(miniurl_list)
