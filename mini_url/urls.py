3f042efb1136194df8655d225c84256f0e322414
chrislo42 <chris42.lopez@gmail.com>
2017-01-21 21:28:46 +0100

    Ajout des boutons cancel Correction miniurl/urls.py ( pas de + dans la regex de edit et remove et ajout du slash pour la pagination )

--- mini_url/urls.py
+++ mini_url/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^url/$', views.miniurl_list, name='miniurl_list'),
-    url(r'^url/(?P<page>\d+)$', views.miniurl_list, name='miniurl_list'),
+    url(r'^url/(?P<page>\d+)/$', views.miniurl_list, name='miniurl_list'),
     url(r'^url/new/$', views.miniurl_edit, name='miniurl_edit'),
     url(r'^(?P<code>.+)/$', views.miniurl_redir, name='miniurl_redir'),
-    url(r'^url/edit/(?P<pk>\d)$', views.URLUpdate.as_view(), name='url_update'),
-    url(r'^url/del/(?P<pk>\d)$', views.URLDelete.as_view(), name='url_delete'),
+    url(r'^url/edit/(?P<pk>\d+)$', views.URLUpdate.as_view(), name='url_update'),
+    url(r'^url/del/(?P<pk>\d+)$', views.URLDelete.as_view(), name='url_delete'),
 ]
